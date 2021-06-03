6e2127f561b6e41cfceab4d8221ed518f4f27251
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-03 00:51:30 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,9 +4,10 @@ def find_min_in_nested_arrays(src)
   
   while outer < src.length do
     inner = 0
+    min = src[outer][inner]
     
     while inner < src[outer].length do
-      min = src[outer][inner]
+      
       if src[outer][inner] < min
         min = src[outer][inner]
       end
